70d2d786aab9905ff9e34a1b6851a75e004383d4
Falah <falahpsht1922@gmail.com>
2024-07-04 09:40:08 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'logout.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MY HERO',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LogoutPage(), // Mengatur LogoutPage sebagai halaman awal
+    );
+  }
+}
